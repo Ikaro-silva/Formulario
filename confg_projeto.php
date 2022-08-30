19c07e770ba2711ce83fb34f96c54bcc52874ce8
Ikaro-silva <111320172+Ikaro-silva@users.noreply.github.com>
2022-08-30 09:58:33 -0300

    Create confg_projeto.php

---
+++ confg_projeto.php
@@ -0,0 +1,16 @@
+
+<?php
+    $lcHost='Localhost';
+    $lcUsername='root';
+    $lcPassword='';
+    $lcName='cadastro_1';
+    $conection= new mysqli($lcHost, $lcUsername,$lcPassword,$lcName);
+
+   // if($conection-> connect_errno){
+    //    echo "erro";
+        
+   //}
+   // else{
+    //   echo "ok";
+   // }
+?
